fb8d4d3c8b4e3efa093710863229a89d6d782286
PedroDavid <u19207574@utp.edu.pe>
2023-10-30 15:40:13 -0500

    [UPD-Slimes]
    + en 2 escenas

--- scenes/game/levels/rooms/scene_1/SlimeGreen.gd
+++ scenes/game/levels/rooms/scene_1/SlimeGreen.gd
@@ -13,20 +13,18 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		print(body.name, " slime: body")
 
-		# Accede al nodo del jugador y almacena su script
 		var player = body
 		var player_script = player.get_node("MainCharacterMovement")		
 		var jumpBefore = player_script.jump
 
-		# Verifica si player_script no es nulo
-		if player_script:
+		if player_script:			
+			player_script.hit(1)
 			player_script.jump = jumpBefore + 100
 			await get_tree().create_timer(2).timeout
 			player_script.jump = jumpBefore 
 		else:
-			print("Player script is null")			
+			print("Player: null")			
 		# HealthDashboard.add_life(5)
-		player_script.hit(1)
 		self.queue_free()  # Liberamos la memoria
 
 
