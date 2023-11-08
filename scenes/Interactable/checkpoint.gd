d80863e89cfa17613282de31fab3008554b72f8a
Jennifer Reyes A <123723018+Jennykings@users.noreply.github.com>
2023-11-08 13:19:25 -0500

    archivos

---
+++ scenes/Interactable/checkpoint.gd
@@ -0,0 +1,29 @@
+extends Node2D
+class_name Checkpoint
+
+@export var spawnpoint = false
+
+var activated = false
+
+
+func activate():
+	GameManager.current_checkpoint = self
+	activated = true
+	$AnimationPlayer.play("Activated")
+
+#func _on_area_2d_area_entered(area):
+	#if area.get_parent() is Player && !activated:
+		#activate()
+
+
+#func _on_area_2d_area_entered(area):
+	#if area.is_in_group("player"):
+		#activate()
+
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		activate()
+
